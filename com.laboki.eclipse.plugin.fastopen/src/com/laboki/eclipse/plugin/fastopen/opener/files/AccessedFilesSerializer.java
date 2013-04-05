0744fa8368ea5abccf3ff07efc36d4c17996074d
Lateef Alabi-Oki <mystilleef@gmail.com>
2013-04-05 16:38:37 -0400

    Implement serializer object for accessed files.

---
+++ com.laboki.eclipse.plugin.fastopen/src/com/laboki/eclipse/plugin/fastopen/opener/files/AccessedFilesSerializer.java
@@ -0,0 +1,52 @@
+package com.laboki.eclipse.plugin.fastopen.opener.files;
+
+import java.util.Collection;
+import java.util.List;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+import com.google.common.eventbus.AllowConcurrentEvents;
+import com.google.common.eventbus.Subscribe;
+import com.laboki.eclipse.plugin.fastopen.EventBus;
+import com.laboki.eclipse.plugin.fastopen.Task;
+import com.laboki.eclipse.plugin.fastopen.events.AccessedFilesEvent;
+import com.laboki.eclipse.plugin.fastopen.events.DeserializedAccessedFilesEvent;
+import com.laboki.eclipse.plugin.fastopen.opener.EditorContext;
+
+public final class AccessedFilesSerializer {
+
+	public static final String SERIALIZABLE_FILE_STRING = "accessed_files_list_test.ser";
+
+	public AccessedFilesSerializer() {
+		EditorContext.emptyFile(AccessedFilesSerializer.SERIALIZABLE_FILE_STRING);
+		AccessedFilesSerializer.postEvent();
+	}
+
+	private static void postEvent() {
+		EventBus.post(new DeserializedAccessedFilesEvent(ImmutableList.copyOf(AccessedFilesSerializer.deserialize())));
+	}
+
+	@SuppressWarnings("unchecked")
+	private static List<String> deserialize() {
+		final Object files = EditorContext.deserialize(AccessedFilesSerializer.SERIALIZABLE_FILE_STRING);
+		if (files == null) return Lists.newArrayList();
+		return (List<String>) files;
+	}
+
+	@Subscribe
+	@AllowConcurrentEvents
+	public static void accessedFilesChanged(final AccessedFilesEvent event) {
+		EditorContext.asyncExec(new Task("") {
+
+			@Override
+			public void execute() {
+				AccessedFilesSerializer.serialize(event.getFiles());
+			}
+		});
+	}
+
+	private static void serialize(final Collection<String> files) {
+		if (files.size() == 0) return;
+		EditorContext.serialize(AccessedFilesSerializer.SERIALIZABLE_FILE_STRING, files);
+	}
+}
