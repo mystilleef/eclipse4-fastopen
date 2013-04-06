0f211ec3c8552a14bd4b6ade56189a5b163b55a0
Lateef Alabi-Oki <mystilleef@gmail.com>
2013-04-06 01:48:09 -0400

    Cache workspace IFiles and use them for verification.

--- com.laboki.eclipse.plugin.fastopen/src/com/laboki/eclipse/plugin/fastopen/opener/RecentResources.java
+++ com.laboki.eclipse.plugin.fastopen/src/com/laboki/eclipse/plugin/fastopen/opener/RecentResources.java
@@ -6,7 +6,10 @@ import java.util.Map;
 
 import lombok.Synchronized;
 
+import org.eclipse.core.resources.IFile;
+
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.eventbus.AllowConcurrentEvents;
@@ -14,10 +17,12 @@ import com.google.common.eventbus.Subscribe;
 import com.laboki.eclipse.plugin.fastopen.EventBus;
 import com.laboki.eclipse.plugin.fastopen.Task;
 import com.laboki.eclipse.plugin.fastopen.events.FileResourcesEvent;
+import com.laboki.eclipse.plugin.fastopen.events.FileResourcesMapEvent;
 import com.laboki.eclipse.plugin.fastopen.events.RecentFilesEvent;
 
 public final class RecentResources {
 
+	private ImmutableMap<String, IFile> fileResourcesMap;
 	private final Map<String, File> cachedResourceFiles = Maps.newHashMap();
 	private final List<File> fileResources = Lists.newArrayList();
 
@@ -46,9 +51,16 @@ public final class RecentResources {
 	}
 
 	private void addNewResourceFiles(final ArrayList<File> files, final String filePath) {
-		final File file = new File(filePath);
+		if (!this.fileResourcesMap.containsKey(filePath)) return;
+		final File file = new File(this.fileResourcesMap.get(filePath));
 		this.cachedResourceFiles.put(filePath, file);
 		files.add(file);
+	}
+
+	@Subscribe
+	@AllowConcurrentEvents
+	public void fileResourcesMap(final FileResourcesMapEvent event) {
+		RecentResources.this.fileResourcesMap = event.getMap();
 	}
 
 	@Subscribe
