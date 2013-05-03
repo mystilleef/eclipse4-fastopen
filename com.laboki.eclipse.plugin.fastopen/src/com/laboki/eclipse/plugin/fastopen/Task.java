399e387ae3545bad0b3351d29187f68f3a27e023
Lateef Alabi-Oki <mystilleef@gmail.com>
2013-05-03 08:48:16 -0400

    Add new Task API.

--- com.laboki.eclipse.plugin.fastopen/src/com/laboki/eclipse/plugin/fastopen/Task.java
+++ com.laboki.eclipse.plugin.fastopen/src/com/laboki/eclipse/plugin/fastopen/Task.java
@@ -1,5 +1,6 @@
 package com.laboki.eclipse.plugin.fastopen;
 
+
 public class Task extends AbstractTask implements Instance {
 
 	public Task() {
@@ -8,6 +9,10 @@ public class Task extends AbstractTask implements Instance {
 
 	public Task(final String name) {
 		super(name, 0, AbstractTask.TASK_INTERACTIVE);
+	}
+
+	public Task(final int delayTime) {
+		super("", delayTime, AbstractTask.TASK_DECORATE);
 	}
 
 	public Task(final String name, final int delayTime) {
